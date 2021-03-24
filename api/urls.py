b3d75649a2528f0aeca9988737a59744ac647f2b
Ritis <jimmyelf@gmail.com>
2021-03-25 01:04:36 +0500

    New UserModel, UserManage, Serializers and setting(DRF, CORE, JWT)

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+        TokenObtainPairView,
+        TokenRefreshView,
+    )
 
 from .views import UserViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register('v1/users', UserViewSet, basename='user')
+router_v1.register('users', UserViewSet, basename='user')
 
-urlpatterns = [path('', include(router_v1.urls)),]
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
